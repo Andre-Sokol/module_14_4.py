c49081d3c1bcad3105a85a5452665afb0607d121
Andre-Sokol <tehburoandre@mail.ru>
2024-12-17 13:15:03 +0500

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,30 +1,24 @@
 import sqlite3
 
-connection = sqlite3.connect('Products.db')
-cursor = connection.cursor()
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Products(
-id INTEGER PRIMARY KEY,
-title TEXT NOT NULL,
-description TEXT,
-price INTEGER NOT NULL
-)
-''')
-
-
-# создаём таблицу Products, если она ещё не создана при помощи SQL-запроса
 def initiate_db():
     connection = sqlite3.connect('Products.db')
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM Products")
-    prod = cursor.fetchone()
-    if prod is None:
-        for i in range(1, 5):
-            cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
-                           (f'Продукт{i}', f'Описание{i}', f'{i * 100}'))
-            connection.commit()  # сохраняем состояние
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    )
+    ''')
+
+
+    for i in range(1, 5):
+        cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
+                       (f'Продукт{i}', f'Описание{i}', f'{i * 100}'))
+        connection.commit()  # сохраняем состояние
+    connection.close()  # закрываем соединение
 
 
 # которая возвращает все записи из таблицы Products
@@ -37,5 +31,5 @@ def get_all_product():
     return cursor.fetchall()
 
 
-connection.commit()  # сохраняем состояние
-connection.close()  # закрываем соединение
+
+
